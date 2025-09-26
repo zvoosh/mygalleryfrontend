f4aae61c66bba876c0195e9b248d7da620b415b5
Dusan Ilic <dusan.ilic1999@gmail.com>
2025-09-26 07:40:47 +0200

    fix: fix

--- src/api/hooks.ts
+++ src/api/hooks.ts
@@ -1,12 +1,12 @@
 const username = import.meta.env.VITE_API_USERNAME;
 const password = import.meta.env.VITE_API_PASSWORD;
-const url = import.meta.env.VITE_API_URL;
 
 const credentials = btoa(`${username}:${password}`);
 
 export const fetchFiles = async () => {
-  const res = await fetch(`${url}/files`, {
+  const res = await fetch("https://backendserver.dusanprogram.eu/files", {
     method: "GET",
+    credentials: "include",
     headers: {
       Authorization: `Basic ${credentials}`,
       "Content-Type": "application/json",
@@ -17,13 +17,15 @@ export const fetchFiles = async () => {
 };
 
 export const fetchSingleFile = async (fileName: string) => {
-  const res = await fetch(`${url}/files/${fileName}`, {
-    method: "GET",
-    headers: {
-      Authorization: `Basic ${credentials}`,
-      "Content-Type": "application/json",
-    },
-  });
+  const res = await fetch(`https://backendserver.dusanprogram.eu/files${fileName}`, {
+  method: "GET",
+  credentials: "include",
+  headers: {
+    Authorization: `Basic ${credentials}`,
+    "Content-Type": "application/json",
+  },
+});
+;
   if (!res.ok) throw new Error("Failed to fetch file content");
   return await res.text();
 };
